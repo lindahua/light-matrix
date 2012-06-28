5b4043fd3b7a917b44bd5ac8eba826be38e4ce47
Dahua Lin <lindahua@gmail.com>
2012-06-28 00:23:58 -0400

    import matrix_fwd and matrix_meta from bcslib

---
+++ light_mat/matrix/matrix_fwd.h
@@ -0,0 +1,103 @@
+/**
+ * @file matrix_fwd.h
+ *
+ * Forward declaration of Matrix-related classes
+ *
+ * @author Dahua Lin
+ */
+
+#ifdef _MSC_VER
+#pragma once
+#endif
+
+#ifndef LIGHTMAT_MATRIX_FWD_H_
+#define LIGHTMAT_MATRIX_FWD_H_
+
+#include <light_mat/core/basic_defs.h>
+
+namespace lmat
+{
+	const int DynamicDim = 0;
+
+	// forward declaration of concepts
+
+
+	/****************************************************************
+	 *
+	 *   A specialized version of matrix_traits<C> must be provided,
+	 *   which should contain the following static members:
+	 *
+	 *   - num_dimensions:	an int value, which must be set to 2
+	 *   					(reserved for future extension)
+	 *
+	 *   - compile_time_num_rows:	compile-time number of rows
+	 *   - compile_time_num_cols:	compile-time number of columns
+	 *   - is_readonly:				whether the contents can be modified
+	 *
+	 *	 - value_type:			the type of element value
+	 *
+	 ****************************************************************/
+
+	template<class Derived> struct matrix_traits;
+
+	template<class Derived, typename T> class IMatrixXpr;
+	template<class Derived, typename T> class IMatrixView;
+	template<class Derived, typename T> class IDenseMatrix;
+
+	// forward declaration of some important types
+
+	template<typename T, int CTRows=DynamicDim, int CTCols=DynamicDim> class dense_matrix; // guranteed to be aligned
+	template<typename T, int CTRows=DynamicDim> class dense_col;
+	template<typename T, int CTCols=DynamicDim> class dense_row;
+
+	template<typename T, int RowDim=DynamicDim, int ColDim=DynamicDim> class cref_matrix;
+	template<typename T, int RowDim=DynamicDim, int ColDim=DynamicDim> class ref_matrix;
+
+	template<typename T, int RowDim=DynamicDim, int ColDim=DynamicDim> class aligned_cref_matrix;
+	template<typename T, int RowDim=DynamicDim, int ColDim=DynamicDim> class aligned_ref_matrix;
+
+	template<typename T, int CTRows=DynamicDim> class cref_col;
+	template<typename T, int CTRows=DynamicDim> class ref_col;
+	template<typename T, int CTCols=DynamicDim> class cref_row;
+	template<typename T, int CTCols=DynamicDim> class ref_row;
+
+	template<typename T, int CTRows=DynamicDim, int CTCols=DynamicDim> class cref_matrix_ex;
+	template<typename T, int CTRows=DynamicDim, int CTCols=DynamicDim> class ref_matrix_ex;
+
+	template<typename T, int CTRows=DynamicDim, int CTCols=DynamicDim> class cref_grid2d;
+	template<typename T, int CTRows=DynamicDim, int CTCols=DynamicDim> class ref_grid2d;
+
+	// auxiliary structures
+
+	template<class Mat, typename RowRange> struct colviews;
+	template<class Mat, typename ColRange> struct rowviews;
+	template<class Mat, typename RowRange, typename ColRange> struct subviews;
+
+	template<class Mat> struct transposed;
+	template<class Mat> class transpose_expr;
+
+	template<class Mat> struct mat_evaluator;
+
+	// forward declaration of useful memory operations
+
+	template<typename T, class LMat, class RMat>
+	inline bool is_equal(const IMatrixView<LMat, T>& A, const IMatrixView<RMat, T>& B);
+
+	template<typename T, class LMat, class RMat>
+	inline bool is_approx(const IMatrixView<LMat, T>& A, const IMatrixView<RMat, T>& B, const T& tol);
+
+	template<typename T, class SExpr, class DMat>
+	inline void evaluate_to(const IMatrixXpr<SExpr, T>& src, IDenseMatrix<DMat, T>& dst);
+
+	template<typename T, class SMat, class DMat>
+	inline void copy(const IMatrixView<SMat, T>& src, IDenseMatrix<DMat, T>& dst);
+
+	template<typename T, class DMat>
+	inline void fill(IDenseMatrix<DMat, T>& dst, const T& v);
+
+	template<typename T, class DMat>
+	inline void zero(IDenseMatrix<DMat, T>& dst);
+}
+
+
+#endif
---
+++ light_mat/matrix/matrix_meta.h
@@ -0,0 +1,217 @@
+/**
+ * @file matrix_meta.h
+ *
+ * Meta programming tools for Matrix
+ *
+ * @author Dahua Lin
+ */
+
+#ifdef _MSC_VER
+#pragma once
+#endif
+
+#ifndef LIGHTMAT_MATRIX_META_H_
+#define LIGHTMAT_MATRIX_META_H_
+
+#include <light_mat/matrix/matrix_fwd.h>
+
+namespace lmat
+{
+	/********************************************
+	 *
+	 *  Dimension related tools
+	 *
+	 ********************************************/
+
+	template<class Mat>
+	struct ct_rows
+	{
+		static const int value = matrix_traits<Mat>::compile_time_num_rows;
+	};
+
+	template<class Mat>
+	struct ct_cols
+	{
+		static const int value = matrix_traits<Mat>::compile_time_num_cols;
+	};
+
+	template<class Mat>
+	struct ct_size
+	{
+		static const int value = ct_rows<Mat>::value * ct_cols<Mat>::value;
+	};
+
+
+	template<int N1, int N2>
+	struct binary_ctdim
+	{
+#ifdef LMAT_USE_STATIC_ASSERT
+		static_assert(!(N1 > 0 && N2 > 0 && N1 != N2), "Incompatible compile-time dimensions.");
+#endif
+		static const int value = N1 > N2 ? N1 : N2;
+	};
+
+
+	template<class Mat1, class Mat2>
+	struct binary_ct_rows
+	{
+		static const int value = binary_ctdim<ct_rows<Mat1>::value, ct_rows<Mat2>::value>::value;
+	};
+
+	template<class Mat1, class Mat2>
+	struct binary_ct_cols
+	{
+		static const int value = binary_ctdim<ct_cols<Mat1>::value, ct_cols<Mat2>::value>::value;
+	};
+
+	template<class Mat1, class Mat2>
+	struct binary_ct_size
+	{
+		static const int value = binary_ct_rows<Mat1, Mat2>::value * binary_ct_cols<Mat1, Mat2>::value;
+	};
+
+
+	template<class Mat>
+	struct ct_is_row
+	{
+		static const bool value = ct_rows<Mat>::value == 1;
+	};
+
+	template<class Mat>
+	struct ct_is_col
+	{
+		static const bool value = ct_cols<Mat>::value == 1;
+	};
+
+	template<class Mat>
+	struct ct_is_vector
+	{
+		static const bool value = ct_is_row<Mat>::value || ct_is_col<Mat>::value;
+	};
+
+	template<class Mat>
+	struct has_static_nrows
+	{
+		static const bool value = ct_rows<Mat>::value > 0;
+	};
+
+	template<class Mat>
+	struct has_static_ncols
+	{
+		static const bool value = ct_cols<Mat>::value > 0;
+	};
+
+	template<class Mat>
+	struct has_dynamic_nrows
+	{
+		static const bool value = ct_rows<Mat>::value == DynamicDim;
+	};
+
+	template<class Mat>
+	struct has_dynamic_ncols
+	{
+		static const bool value = ct_cols<Mat>::value == DynamicDim;
+	};
+
+
+	template<class Mat>
+	struct has_static_size
+	{
+		static const bool value = has_static_nrows<Mat>::value && has_static_ncols<Mat>::value;
+	};
+
+	template<class LMat, class RMat>
+	struct ct_has_same_nrows
+	{
+		static const bool value =
+				ct_rows<LMat>::value > 0 &&
+				ct_rows<LMat>::value == ct_rows<RMat>::value;
+	};
+
+	template<class LMat, class RMat>
+	struct ct_has_same_ncols
+	{
+		static const bool value =
+				ct_cols<LMat>::value > 0 &&
+				ct_cols<LMat>::value == ct_cols<RMat>::value;
+	};
+
+
+	/********************************************
+	 *
+	 *  Concept test tools
+	 *
+	 ********************************************/
+
+	template<class T>
+	struct is_supported_matrix_value_type
+	{
+		static const bool value = lmat::is_pod<T>::value;
+	};
+
+	template<class Derived, template<class D, typename T> class Interface>
+	struct has_matrix_interface
+	{
+		typedef Interface<Derived, typename matrix_traits<Derived>::value_type> expect_base;
+		static const bool value = is_base_of<expect_base, Derived>::value;
+	};
+
+	template<class Mat>
+	struct is_mat_xpr
+	{
+		static const bool value = has_matrix_interface<Mat, IMatrixXpr>::value;
+	};
+
+	template<class Mat>
+	struct is_mat_view
+	{
+		static const bool value = has_matrix_interface<Mat, IMatrixView>::value;
+	};
+
+	template<class Mat>
+	struct is_dense_mat
+	{
+		static const bool value = has_matrix_interface<Mat, IDenseMatrix>::value;
+	};
+
+
+	/********************************************
+	 *
+	 *  Matrix access and manipulation
+	 *
+	 ********************************************/
+
+	template<class Mat>
+	struct is_readonly_mat
+	{
+		static const bool value = matrix_traits<Mat>::is_readonly;
+	};
+
+	template<class Mat>
+	struct mat_access
+	{
+		typedef typename matrix_traits<Mat>::value_type value_type;
+		typedef typename if_<is_readonly_mat<Mat>, const value_type, value_type> access_type;
+
+		typedef access_type* pointer;
+		typedef access_type& reference;
+	};
+
+
+	/********************************************
+	 *
+	 *  Default attributes
+	 *
+	 ********************************************/
+
+	template<class Mat>
+	struct has_continuous_layout { static const bool value = false; };
+
+	template<class Mat>
+	struct is_base_aligned { static const bool value = false; };
+
+	template<class Mat>
+	struct is_linear_accessible { static const bool value = false; };
+}
+
+#endif /* MATRIX_META_H_ */
