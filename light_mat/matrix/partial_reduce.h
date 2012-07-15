f197f884ec53591f4e5d84bbb12f5b06e9cd4426
Dahua Lin <lindahua@gmail.com>
2012-07-14 23:03:11 -0400

    partial réduction tested

--- light_mat/matrix/partial_reduce.h
+++ light_mat/matrix/partial_reduce.h
@@ -178,7 +178,7 @@ namespace lmat
 	template<class Fun, class Arg>
 	LMAT_ENSURE_INLINE
 	inline typename colwise_reduce_expr_map<Fun, Arg, false>::type
-	reduce(  const Fun& fun,
+	reduce( const Fun& fun,
 			const IMatrixXpr<Arg, typename Fun::arg_type>& arg,
 			colwise )
 	{
@@ -187,12 +187,32 @@ namespace lmat
 
 	template<class Fun, class Arg>
 	LMAT_ENSURE_INLINE
+	inline typename colwise_reduce_expr_map<Fun, Arg, true>::type
+	reduce( const Fun& fun,
+			const embed_mat<Arg, typename Fun::arg_type>& arg,
+			colwise )
+	{
+		return colwise_reduce_expr<Fun, Arg, true>(fun, arg.get());
+	}
+
+	template<class Fun, class Arg>
+	LMAT_ENSURE_INLINE
 	inline typename rowwise_reduce_expr_map<Fun, Arg, false>::type
-	reduce(  const Fun& fun,
+	reduce( const Fun& fun,
 			const IMatrixXpr<Arg, typename Fun::arg_type>& arg,
 			rowwise )
 	{
 		return rowwise_reduce_expr<Fun, Arg, false>(fun, arg.derived());
+	}
+
+	template<class Fun, class Arg>
+	LMAT_ENSURE_INLINE
+	inline typename rowwise_reduce_expr_map<Fun, Arg, true>::type
+	reduce( const Fun& fun,
+			const embed_mat<Arg, typename Fun::arg_type>& arg,
+			rowwise )
+	{
+		return rowwise_reduce_expr<Fun, Arg, true>(fun, arg.get());
 	}
 
 
@@ -241,13 +261,29 @@ namespace lmat
 	inline typename colwise_sum_expr_map<Arg, false>::type
 	sum(const IMatrixXpr<Arg, T>& arg, colwise)
 	{
-		return reduce(sum_fun<T>(), arg, colwise());
+		return reduce(sum_fun<T>(), arg.derived(), colwise());
 	}
 
 	template<typename T, class Arg>
 	LMAT_ENSURE_INLINE
 	inline typename rowwise_sum_expr_map<Arg, false>::type
 	sum(const IMatrixXpr<Arg, T>& arg, rowwise)
+	{
+		return reduce(sum_fun<T>(), arg.derived(), rowwise());
+	}
+
+	template<typename T, class Arg>
+	LMAT_ENSURE_INLINE
+	inline typename colwise_sum_expr_map<Arg, true>::type
+	sum(const embed_mat<Arg, T>& arg, colwise)
+	{
+		return reduce(sum_fun<T>(), arg, colwise());
+	}
+
+	template<typename T, class Arg>
+	LMAT_ENSURE_INLINE
+	inline typename rowwise_sum_expr_map<Arg, true>::type
+	sum(const embed_mat<Arg, T>& arg, rowwise)
 	{
 		return reduce(sum_fun<T>(), arg, rowwise());
 	}
@@ -278,7 +314,7 @@ namespace lmat
 	inline typename colwise_mean_expr_map<Arg, false>::type
 	mean(const IMatrixXpr<Arg, T>& arg, colwise)
 	{
-		return embed(sum(arg, colwise())) *
+		return embed(sum(arg.derived(), colwise())) *
 				math::rcp(T(arg.nrows()));
 	}
 
@@ -287,8 +323,396 @@ namespace lmat
 	inline typename rowwise_sum_expr_map<Arg, false>::type
 	mean(const IMatrixXpr<Arg, T>& arg, rowwise)
 	{
-		return embed(sum(arg, rowwise())) *
+		return embed(sum(arg.derived(), rowwise())) *
 				math::rcp(T(arg.ncolumns()));
+	}
+
+
+	// prod
+
+	template<class Arg, bool EmbedArg>
+	struct colwise_prod_expr_map
+	{
+		typedef typename colwise_reduce_expr_map<
+				prod_fun<typename matrix_traits<Arg>::value_type>,
+				Arg, EmbedArg>::type type;
+	};
+
+	template<class Arg, bool EmbedArg>
+	struct rowwise_prod_expr_map
+	{
+		typedef typename rowwise_reduce_expr_map<
+				prod_fun<typename matrix_traits<Arg>::value_type>,
+				Arg, EmbedArg>::type type;
+	};
+
+	template<typename T, class Arg>
+	LMAT_ENSURE_INLINE
+	inline typename colwise_prod_expr_map<Arg, false>::type
+	prod(const IMatrixXpr<Arg, T>& arg, colwise)
+	{
+		return reduce(prod_fun<T>(), arg.derived(), colwise());
+	}
+
+	template<typename T, class Arg>
+	LMAT_ENSURE_INLINE
+	inline typename rowwise_prod_expr_map<Arg, false>::type
+	prod(const IMatrixXpr<Arg, T>& arg, rowwise)
+	{
+		return reduce(prod_fun<T>(), arg.derived(), rowwise());
+	}
+
+	template<typename T, class Arg>
+	LMAT_ENSURE_INLINE
+	inline typename colwise_prod_expr_map<Arg, true>::type
+	prod(const embed_mat<Arg, T>& arg, colwise)
+	{
+		return reduce(prod_fun<T>(), arg, colwise());
+	}
+
+	template<typename T, class Arg>
+	LMAT_ENSURE_INLINE
+	inline typename rowwise_prod_expr_map<Arg, true>::type
+	prod(const embed_mat<Arg, T>& arg, rowwise)
+	{
+		return reduce(prod_fun<T>(), arg, rowwise());
+	}
+
+
+	// maximum
+
+	template<class Arg, bool EmbedArg>
+	struct colwise_maximum_expr_map
+	{
+		typedef typename colwise_reduce_expr_map<
+				maximum_fun<typename matrix_traits<Arg>::value_type>,
+				Arg, EmbedArg>::type type;
+	};
+
+	template<class Arg, bool EmbedArg>
+	struct rowwise_maximum_expr_map
+	{
+		typedef typename rowwise_reduce_expr_map<
+				maximum_fun<typename matrix_traits<Arg>::value_type>,
+				Arg, EmbedArg>::type type;
+	};
+
+	template<typename T, class Arg>
+	LMAT_ENSURE_INLINE
+	inline typename colwise_maximum_expr_map<Arg, false>::type
+	maximum(const IMatrixXpr<Arg, T>& arg, colwise)
+	{
+		return reduce(maximum_fun<T>(), arg.derived(), colwise());
+	}
+
+	template<typename T, class Arg>
+	LMAT_ENSURE_INLINE
+	inline typename rowwise_maximum_expr_map<Arg, false>::type
+	maximum(const IMatrixXpr<Arg, T>& arg, rowwise)
+	{
+		return reduce(maximum_fun<T>(), arg.derived(), rowwise());
+	}
+
+	template<typename T, class Arg>
+	LMAT_ENSURE_INLINE
+	inline typename colwise_maximum_expr_map<Arg, true>::type
+	maximum(const embed_mat<Arg, T>& arg, colwise)
+	{
+		return reduce(maximum_fun<T>(), arg, colwise());
+	}
+
+	template<typename T, class Arg>
+	LMAT_ENSURE_INLINE
+	inline typename rowwise_maximum_expr_map<Arg, true>::type
+	maximum(const embed_mat<Arg, T>& arg, rowwise)
+	{
+		return reduce(maximum_fun<T>(), arg, rowwise());
+	}
+
+
+	// minimum
+
+	template<class Arg, bool EmbedArg>
+	struct colwise_minimum_expr_map
+	{
+		typedef typename colwise_reduce_expr_map<
+				minimum_fun<typename matrix_traits<Arg>::value_type>,
+				Arg, EmbedArg>::type type;
+	};
+
+	template<class Arg, bool EmbedArg>
+	struct rowwise_minimum_expr_map
+	{
+		typedef typename rowwise_reduce_expr_map<
+				minimum_fun<typename matrix_traits<Arg>::value_type>,
+				Arg, EmbedArg>::type type;
+	};
+
+	template<typename T, class Arg>
+	LMAT_ENSURE_INLINE
+	inline typename colwise_minimum_expr_map<Arg, false>::type
+	minimum(const IMatrixXpr<Arg, T>& arg, colwise)
+	{
+		return reduce(minimum_fun<T>(), arg.derived(), colwise());
+	}
+
+	template<typename T, class Arg>
+	LMAT_ENSURE_INLINE
+	inline typename rowwise_minimum_expr_map<Arg, false>::type
+	minimum(const IMatrixXpr<Arg, T>& arg, rowwise)
+	{
+		return reduce(minimum_fun<T>(), arg.derived(), rowwise());
+	}
+
+	template<typename T, class Arg>
+	LMAT_ENSURE_INLINE
+	inline typename colwise_minimum_expr_map<Arg, true>::type
+	minimum(const embed_mat<Arg, T>& arg, colwise)
+	{
+		return reduce(minimum_fun<T>(), arg, colwise());
+	}
+
+	template<typename T, class Arg>
+	LMAT_ENSURE_INLINE
+	inline typename rowwise_minimum_expr_map<Arg, true>::type
+	minimum(const embed_mat<Arg, T>& arg, rowwise)
+	{
+		return reduce(minimum_fun<T>(), arg, rowwise());
+	}
+
+
+
+	// dot
+
+	template<class LArg, class RArg, bool EmbedLArg, bool EmbedRArg>
+	struct colwise_dot_expr_map
+	{
+		typedef typename colwise_sum_expr_map<
+					typename mul_expr_map<LArg, RArg, EmbedLArg, EmbedRArg>::type,
+					true
+				>::type type;
+	};
+
+	template<class LArg, class RArg, bool EmbedLArg, bool EmbedRArg>
+	struct rowwise_dot_expr_map
+	{
+		typedef typename rowwise_sum_expr_map<
+					typename mul_expr_map<LArg, RArg, EmbedLArg, EmbedRArg>::type,
+					true
+				>::type type;
+	};
+
+	template<typename T, class LArg, class RArg>
+	LMAT_ENSURE_INLINE
+	inline typename colwise_dot_expr_map<LArg, RArg, false, false>::type
+	dot(const IMatrixXpr<LArg, T>& a, const IMatrixXpr<RArg, T>& b, colwise)
+	{
+		return sum(embed(a.derived() * b.derived()), colwise());
+	}
+
+	template<typename T, class LArg, class RArg>
+	LMAT_ENSURE_INLINE
+	inline typename rowwise_dot_expr_map<LArg, RArg, false, false>::type
+	dot(const IMatrixXpr<LArg, T>& a, const IMatrixXpr<RArg, T>& b, rowwise)
+	{
+		return sum(embed(a.derived() * b.derived()), rowwise());
+	}
+
+
+	// L1norm
+
+	template<class Arg, bool EmbedArg>
+	struct colwise_L1norm_expr_map
+	{
+		typedef typename colwise_sum_expr_map<
+					typename abs_expr_map<Arg, EmbedArg>::type,
+					true
+				>::type type;
+	};
+
+	template<class Arg, bool EmbedArg>
+	struct rowwise_L1norm_expr_map
+	{
+		typedef typename rowwise_sum_expr_map<
+					typename abs_expr_map<Arg, EmbedArg>::type,
+					true
+				>::type type;
+	};
+
+	template<typename T, class Arg>
+	LMAT_ENSURE_INLINE
+	inline typename colwise_L1norm_expr_map<Arg, false>::type
+	L1norm(const IMatrixXpr<Arg, T>& arg, colwise)
+	{
+		return sum(embed(abs(arg.derived())), colwise());
+	}
+
+	template<typename T, class Arg>
+	LMAT_ENSURE_INLINE
+	inline typename rowwise_L1norm_expr_map<Arg, false>::type
+	L1norm(const IMatrixXpr<Arg, T>& arg, rowwise)
+	{
+		return sum(embed(abs(arg.derived())), rowwise());
+	}
+
+
+	// sqL2norm
+
+	template<class Arg, bool EmbedArg>
+	struct colwise_sqL2norm_expr_map
+	{
+		typedef typename colwise_sum_expr_map<
+					typename sqr_expr_map<Arg, EmbedArg>::type,
+					true
+				>::type type;
+	};
+
+	template<class Arg, bool EmbedArg>
+	struct rowwise_sqL2norm_expr_map
+	{
+		typedef typename rowwise_sum_expr_map<
+					typename sqr_expr_map<Arg, EmbedArg>::type,
+					true
+				>::type type;
+	};
+
+	template<typename T, class Arg>
+	LMAT_ENSURE_INLINE
+	inline typename colwise_sqL2norm_expr_map<Arg, false>::type
+	sqL2norm(const IMatrixXpr<Arg, T>& arg, colwise)
+	{
+		return sum(embed(sqr(arg.derived())), colwise());
+	}
+
+	template<typename T, class Arg>
+	LMAT_ENSURE_INLINE
+	inline typename rowwise_sqL2norm_expr_map<Arg, false>::type
+	sqL2norm(const IMatrixXpr<Arg, T>& arg, rowwise)
+	{
+		return sum(embed(sqr(arg.derived())), rowwise());
+	}
+
+
+	// L2norm
+
+	template<class Arg, bool EmbedArg>
+	struct colwise_L2norm_expr_map
+	{
+		typedef typename sqrt_expr_map<
+					typename colwise_sqL2norm_expr_map<Arg, EmbedArg>::type,
+					true
+				>::type type;
+	};
+
+	template<class Arg, bool EmbedArg>
+	struct rowwise_L2norm_expr_map
+	{
+		typedef typename sqrt_expr_map<
+					typename rowwise_sqL2norm_expr_map<Arg, EmbedArg>::type,
+					true
+				>::type type;
+	};
+
+	template<typename T, class Arg>
+	LMAT_ENSURE_INLINE
+	inline typename colwise_L2norm_expr_map<Arg, false>::type
+	L2norm(const IMatrixXpr<Arg, T>& arg, colwise)
+	{
+		return sqrt(embed(sqL2norm(arg.derived(), colwise())));
+	}
+
+	template<typename T, class Arg>
+	LMAT_ENSURE_INLINE
+	inline typename rowwise_L2norm_expr_map<Arg, false>::type
+	L2norm(const IMatrixXpr<Arg, T>& arg, rowwise)
+	{
+		return sqrt(embed(sqL2norm(arg.derived(), rowwise())));
+	}
+
+
+	// Linfnorm
+
+	template<class Arg, bool EmbedArg>
+	struct colwise_Linfnorm_expr_map
+	{
+		typedef typename colwise_maximum_expr_map<
+					typename abs_expr_map<Arg, EmbedArg>::type,
+					true
+				>::type type;
+	};
+
+	template<class Arg, bool EmbedArg>
+	struct rowwise_Linfnorm_expr_map
+	{
+		typedef typename rowwise_maximum_expr_map<
+					typename abs_expr_map<Arg, EmbedArg>::type,
+					true
+				>::type type;
+	};
+
+	template<typename T, class Arg>
+	LMAT_ENSURE_INLINE
+	inline typename colwise_Linfnorm_expr_map<Arg, false>::type
+	Linfnorm(const IMatrixXpr<Arg, T>& arg, colwise)
+	{
+		return maximum(embed(abs(arg.derived())), colwise());
+	}
+
+	template<typename T, class Arg>
+	LMAT_ENSURE_INLINE
+	inline typename rowwise_Linfnorm_expr_map<Arg, false>::type
+	Linfnorm(const IMatrixXpr<Arg, T>& arg, rowwise)
+	{
+		return maximum(embed(abs(arg.derived())), rowwise());
+	}
+
+
+	// nrmdot
+
+	template<class LArg, class RArg, bool EmbedLArg, bool EmbedRArg>
+	struct colwise_nrmdot_expr_map
+	{
+		typedef typename div_expr_map<
+					typename colwise_dot_expr_map<LArg, RArg, EmbedLArg, EmbedRArg>::type,
+					typename mul_expr_map<
+						typename colwise_L2norm_expr_map<LArg, EmbedLArg>::type,
+						typename colwise_L2norm_expr_map<RArg, EmbedRArg>::type,
+					true, true>::type,
+				true, true>::type type;
+	};
+
+	template<class LArg, class RArg, bool EmbedLArg, bool EmbedRArg>
+	struct rowwise_nrmdot_expr_map
+	{
+		typedef typename div_expr_map<
+					typename rowwise_dot_expr_map<LArg, RArg, EmbedLArg, EmbedRArg>::type,
+					typename mul_expr_map<
+						typename rowwise_L2norm_expr_map<LArg, EmbedLArg>::type,
+						typename rowwise_L2norm_expr_map<RArg, EmbedRArg>::type,
+					true, true>::type,
+				true, true>::type type;
+	};
+
+	template<typename T, class LArg, class RArg>
+	LMAT_ENSURE_INLINE
+	inline typename colwise_nrmdot_expr_map<LArg, RArg, false, false>::type
+	nrmdot(const IMatrixXpr<LArg, T>& a, const IMatrixXpr<RArg, T>& b, colwise)
+	{
+		return embed(dot(a.derived(), b.derived(), colwise())) /
+				embed(
+						embed(L2norm(a.derived(), colwise())) *
+						embed(L2norm(b.derived(), colwise())) );
+	}
+
+	template<typename T, class LArg, class RArg>
+	LMAT_ENSURE_INLINE
+	inline typename rowwise_nrmdot_expr_map<LArg, RArg, false, false>::type
+	nrmdot(const IMatrixXpr<LArg, T>& a, const IMatrixXpr<RArg, T>& b, rowwise)
+	{
+		return embed(dot(a.derived(), b.derived(), rowwise())) /
+				embed(
+						embed(L2norm(a.derived(), rowwise())) *
+						embed(L2norm(b.derived(), rowwise())) );
 	}
 
 }
--- tests/test_partial_reduce.cpp
+++ tests/test_partial_reduce.cpp
@@ -108,6 +108,573 @@ MN_CASE( colwise_reduce, mean )
 }
 
 
+MN_CASE( rowwise_reduce, mean )
+{
+	typedef dense_matrix<double, M, N> mat_t;
+	typedef dense_col<double, M> col_t;
+
+	const index_t m = M == 0 ? DM : M;
+	const index_t n = N == 0 ? DN : N;
+
+	mat_t A(m, n);
+	for (index_t i = 0; i < m * n; ++i) A[i] = double(i + 1);
+
+	// prepare ground-truth
+
+	col_t r0(m);
+	for (index_t i = 0; i < m; ++i)
+	{
+		double s = 0.0;
+		for (index_t j = 0; j < n; ++j) s += A(i, j);
+		r0[i] = s / double(n);
+	}
+
+	// test
+
+	col_t r = mean(A, rowwise());
+	ASSERT_EQ( r.nrows(), m );
+	ASSERT_EQ( r.ncolumns(), 1 );
+	ASSERT_VEC_APPROX( m, r, r0, 1.0e-12 );
+}
+
+
+MN_CASE( colwise_reduce, maximum )
+{
+	typedef dense_matrix<double, M, N> mat_t;
+	typedef dense_row<double, N> row_t;
+
+	const index_t m = M == 0 ? DM : M;
+	const index_t n = N == 0 ? DN : N;
+
+	mat_t A(m, n);
+	for (index_t i = 0; i < m * n; ++i) A[i] = double((i + 1) * (10 - i));
+
+	// prepare ground-truth
+
+	row_t r0(n);
+	for (index_t j = 0; j < n; ++j)
+	{
+		double s = A(0, j);
+		for (index_t i = 0; i < m; ++i)
+			if (A(i, j) > s) s = A(i, j);
+		r0[j] = s;
+	}
+
+	// test
+
+	row_t r = maximum(A, colwise());
+	ASSERT_EQ( r.nrows(), 1 );
+	ASSERT_EQ( r.ncolumns(), n );
+	ASSERT_VEC_EQ( n, r, r0 );
+}
+
+MN_CASE( rowwise_reduce, maximum )
+{
+	typedef dense_matrix<double, M, N> mat_t;
+	typedef dense_col<double, M> col_t;
+
+	const index_t m = M == 0 ? DM : M;
+	const index_t n = N == 0 ? DN : N;
+
+	mat_t A(m, n);
+	for (index_t i = 0; i < m * n; ++i) A[i] = double((i + 1) * (10 - i));
+
+	// prepare ground-truth
+
+	col_t r0(m);
+	for (index_t i = 0; i < m; ++i)
+	{
+		double s = A(i, 0);
+		for (index_t j = 0; j < n; ++j)
+			if (A(i, j) > s) s = A(i, j);
+		r0[i] = s;
+	}
+
+	// test
+
+	col_t r = maximum(A, rowwise());
+
+	ASSERT_EQ( r.nrows(), m );
+	ASSERT_EQ( r.ncolumns(), 1 );
+	ASSERT_VEC_EQ( m, r, r0 );
+}
+
+
+MN_CASE( colwise_reduce, minimum )
+{
+	typedef dense_matrix<double, M, N> mat_t;
+	typedef dense_row<double, N> row_t;
+
+	const index_t m = M == 0 ? DM : M;
+	const index_t n = N == 0 ? DN : N;
+
+	mat_t A(m, n);
+	for (index_t i = 0; i < m * n; ++i) A[i] = double((i + 1) * (i - 10));
+
+	// prepare ground-truth
+
+	row_t r0(n);
+	for (index_t j = 0; j < n; ++j)
+	{
+		double s = A(0, j);
+		for (index_t i = 0; i < m; ++i)
+			if (A(i, j) < s) s = A(i, j);
+		r0[j] = s;
+	}
+
+	// test
+
+	row_t r = minimum(A, colwise());
+	ASSERT_EQ( r.nrows(), 1 );
+	ASSERT_EQ( r.ncolumns(), n );
+	ASSERT_VEC_EQ( n, r, r0 );
+}
+
+MN_CASE( rowwise_reduce, minimum )
+{
+	typedef dense_matrix<double, M, N> mat_t;
+	typedef dense_col<double, M> col_t;
+
+	const index_t m = M == 0 ? DM : M;
+	const index_t n = N == 0 ? DN : N;
+
+	mat_t A(m, n);
+	for (index_t i = 0; i < m * n; ++i) A[i] = double((i + 1) * (i - 10));
+
+	// prepare ground-truth
+
+	col_t r0(m);
+	for (index_t i = 0; i < m; ++i)
+	{
+		double s = A(i, 0);
+		for (index_t j = 0; j < n; ++j)
+			if (A(i, j) < s) s = A(i, j);
+		r0[i] = s;
+	}
+
+	// test
+
+	col_t r = minimum(A, rowwise());
+
+	ASSERT_EQ( r.nrows(), m );
+	ASSERT_EQ( r.ncolumns(), 1 );
+	ASSERT_VEC_EQ( m, r, r0 );
+}
+
+
+
+MN_CASE( colwise_reduce, dot )
+{
+	typedef dense_matrix<double, M, N> mat_t;
+	typedef dense_row<double, N> row_t;
+
+	const index_t m = M == 0 ? DM : M;
+	const index_t n = N == 0 ? DN : N;
+
+	mat_t A(m, n);
+	mat_t B(m, n);
+	for (index_t i = 0; i < m * n; ++i)
+	{
+		A[i] = double(i + 1);
+		B[i] = double(i + 2);
+	}
+
+	// prepare ground-truth
+
+	row_t r0(n);
+	for (index_t j = 0; j < n; ++j)
+	{
+		double s = 0.0;
+		for (index_t i = 0; i < m; ++i) s += A(i, j) * B(i, j);
+		r0[j] = s;
+	}
+
+	// test
+
+	row_t r = dot(A, B, colwise());
+	ASSERT_EQ( r.nrows(), 1 );
+	ASSERT_EQ( r.ncolumns(), n );
+	ASSERT_VEC_EQ( n, r, r0 );
+}
+
+MN_CASE( rowwise_reduce, dot )
+{
+	typedef dense_matrix<double, M, N> mat_t;
+	typedef dense_col<double, M> col_t;
+
+	const index_t m = M == 0 ? DM : M;
+	const index_t n = N == 0 ? DN : N;
+
+	mat_t A(m, n);
+	mat_t B(m, n);
+	for (index_t i = 0; i < m * n; ++i)
+	{
+		A[i] = double(i + 1);
+		B[i] = double(i + 2);
+	}
+
+	// prepare ground-truth
+
+	col_t r0(m);
+	for (index_t i = 0; i < m; ++i)
+	{
+		double s = 0.0;
+		for (index_t j = 0; j < n; ++j) s += A(i, j) * B(i, j);
+		r0[i] = s;
+	}
+
+	// test
+
+	col_t r = dot(A, B, rowwise());
+	ASSERT_EQ( r.nrows(), m );
+	ASSERT_EQ( r.ncolumns(), 1 );
+	ASSERT_VEC_EQ( m, r, r0 );
+}
+
+
+MN_CASE( colwise_reduce, L1norm )
+{
+	typedef dense_matrix<double, M, N> mat_t;
+	typedef dense_row<double, N> row_t;
+
+	const index_t m = M == 0 ? DM : M;
+	const index_t n = N == 0 ? DN : N;
+
+	mat_t A(m, n);
+	for (index_t i = 0; i < m * n; ++i)
+	{
+		A[i] = i & 1 ? double(i + 1) : double(-(i + 1));
+	}
+
+	// prepare ground-truth
+
+	row_t r0(n);
+	for (index_t j = 0; j < n; ++j)
+	{
+		double s = 0.0;
+		for (index_t i = 0; i < m; ++i) s += std::abs(A(i, j));
+		r0[j] = s;
+	}
+
+	// test
+
+	row_t r = L1norm(A, colwise());
+	ASSERT_EQ( r.nrows(), 1 );
+	ASSERT_EQ( r.ncolumns(), n );
+	ASSERT_VEC_EQ( n, r, r0 );
+}
+
+MN_CASE( rowwise_reduce, L1norm )
+{
+	typedef dense_matrix<double, M, N> mat_t;
+	typedef dense_col<double, M> col_t;
+
+	const index_t m = M == 0 ? DM : M;
+	const index_t n = N == 0 ? DN : N;
+
+	mat_t A(m, n);
+	for (index_t i = 0; i < m * n; ++i)
+	{
+		A[i] = i & 1 ? double(i + 1) : double(-(i + 1));
+	}
+
+	// prepare ground-truth
+
+	col_t r0(m);
+	for (index_t i = 0; i < m; ++i)
+	{
+		double s = 0.0;
+		for (index_t j = 0; j < n; ++j) s += std::abs(A(i, j));
+		r0[i] = s;
+	}
+
+	// test
+
+	col_t r = L1norm(A, rowwise());
+	ASSERT_EQ( r.nrows(), m );
+	ASSERT_EQ( r.ncolumns(), 1 );
+	ASSERT_VEC_EQ( m, r, r0 );
+}
+
+
+
+MN_CASE( colwise_reduce, sqL2norm )
+{
+	typedef dense_matrix<double, M, N> mat_t;
+	typedef dense_row<double, N> row_t;
+
+	const index_t m = M == 0 ? DM : M;
+	const index_t n = N == 0 ? DN : N;
+
+	mat_t A(m, n);
+	for (index_t i = 0; i < m * n; ++i) A[i] = double(i + 1);
+
+	// prepare ground-truth
+
+	row_t r0(n);
+	for (index_t j = 0; j < n; ++j)
+	{
+		double s = 0.0;
+		for (index_t i = 0; i < m; ++i) s += A(i, j) * A(i, j);
+		r0[j] = s;
+	}
+
+	// test
+
+	row_t r = sqL2norm(A, colwise());
+	ASSERT_EQ( r.nrows(), 1 );
+	ASSERT_EQ( r.ncolumns(), n );
+	ASSERT_VEC_EQ( n, r, r0 );
+}
+
+MN_CASE( rowwise_reduce, sqL2norm )
+{
+	typedef dense_matrix<double, M, N> mat_t;
+	typedef dense_col<double, M> col_t;
+
+	const index_t m = M == 0 ? DM : M;
+	const index_t n = N == 0 ? DN : N;
+
+	mat_t A(m, n);
+	for (index_t i = 0; i < m * n; ++i) A[i] = double(i + 1);
+
+	// prepare ground-truth
+
+	col_t r0(m);
+	for (index_t i = 0; i < m; ++i)
+	{
+		double s = 0.0;
+		for (index_t j = 0; j < n; ++j) s += A(i, j) * A(i, j);
+		r0[i] = s;
+	}
+
+	// test
+
+	col_t r = sqL2norm(A, rowwise());
+	ASSERT_EQ( r.nrows(), m );
+	ASSERT_EQ( r.ncolumns(), 1 );
+	ASSERT_VEC_EQ( m, r, r0 );
+}
+
+
+MN_CASE( colwise_reduce, L2norm )
+{
+	typedef dense_matrix<double, M, N> mat_t;
+	typedef dense_row<double, N> row_t;
+
+	const index_t m = M == 0 ? DM : M;
+	const index_t n = N == 0 ? DN : N;
+
+	mat_t A(m, n);
+	for (index_t i = 0; i < m * n; ++i) A[i] = double(i + 1);
+
+	// prepare ground-truth
+
+	row_t r0(n);
+	for (index_t j = 0; j < n; ++j)
+	{
+		double s = 0.0;
+		for (index_t i = 0; i < m; ++i) s += A(i, j) * A(i, j);
+		r0[j] = std::sqrt(s);
+	}
+
+	// test
+
+	row_t r = L2norm(A, colwise());
+	ASSERT_EQ( r.nrows(), 1 );
+	ASSERT_EQ( r.ncolumns(), n );
+	ASSERT_VEC_APPROX( n, r, r0, 1.0e-12 );
+}
+
+MN_CASE( rowwise_reduce, L2norm )
+{
+	typedef dense_matrix<double, M, N> mat_t;
+	typedef dense_col<double, M> col_t;
+
+	const index_t m = M == 0 ? DM : M;
+	const index_t n = N == 0 ? DN : N;
+
+	mat_t A(m, n);
+	for (index_t i = 0; i < m * n; ++i) A[i] = double(i + 1);
+
+	// prepare ground-truth
+
+	col_t r0(m);
+	for (index_t i = 0; i < m; ++i)
+	{
+		double s = 0.0;
+		for (index_t j = 0; j < n; ++j) s += A(i, j) * A(i, j);
+		r0[i] = std::sqrt(s);
+	}
+
+	// test
+
+	col_t r = L2norm(A, rowwise());
+	ASSERT_EQ( r.nrows(), m );
+	ASSERT_EQ( r.ncolumns(), 1 );
+	ASSERT_VEC_APPROX( m, r, r0, 1.0e-12 );
+}
+
+
+MN_CASE( colwise_reduce, Linfnorm )
+{
+	typedef dense_matrix<double, M, N> mat_t;
+	typedef dense_row<double, N> row_t;
+
+	const index_t m = M == 0 ? DM : M;
+	const index_t n = N == 0 ? DN : N;
+
+	mat_t A(m, n);
+	for (index_t i = 0; i < m * n; ++i)
+	{
+		A[i] = i & 1 ? double(i + 1) : double(-(i + 1));
+	}
+
+	// prepare ground-truth
+
+	row_t r0(n);
+	for (index_t j = 0; j < n; ++j)
+	{
+		double s = 0.0;
+		for (index_t i = 0; i < m; ++i)
+		{
+			if ( std::abs(A(i, j)) > s ) s = std::abs(A(i, j));
+		}
+		r0[j] = s;
+	}
+
+	// test
+
+	row_t r = Linfnorm(A, colwise());
+	ASSERT_EQ( r.nrows(), 1 );
+	ASSERT_EQ( r.ncolumns(), n );
+	ASSERT_VEC_EQ( n, r, r0 );
+}
+
+MN_CASE( rowwise_reduce, Linfnorm )
+{
+	typedef dense_matrix<double, M, N> mat_t;
+	typedef dense_col<double, M> col_t;
+
+	const index_t m = M == 0 ? DM : M;
+	const index_t n = N == 0 ? DN : N;
+
+	mat_t A(m, n);
+	for (index_t i = 0; i < m * n; ++i)
+	{
+		A[i] = i & 1 ? double(i + 1) : double(-(i + 1));
+	}
+
+	// prepare ground-truth
+
+	col_t r0(m);
+	for (index_t i = 0; i < m; ++i)
+	{
+		double s = 0.0;
+		for (index_t j = 0; j < n; ++j)
+		{
+			if ( std::abs(A(i, j)) > s ) s = std::abs(A(i, j));
+		}
+		r0[i] = s;
+	}
+
+	// test
+
+	col_t r = Linfnorm(A, rowwise());
+	ASSERT_EQ( r.nrows(), m );
+	ASSERT_EQ( r.ncolumns(), 1 );
+	ASSERT_VEC_EQ( m, r, r0 );
+}
+
+
+
+
+MN_CASE( colwise_reduce, nrmdot )
+{
+	typedef dense_matrix<double, M, N> mat_t;
+	typedef dense_row<double, N> row_t;
+
+	const index_t m = M == 0 ? DM : M;
+	const index_t n = N == 0 ? DN : N;
+
+	mat_t A(m, n);
+	mat_t B(m, n);
+	for (index_t i = 0; i < m * n; ++i)
+	{
+		A[i] = double(i + 1);
+		B[i] = double(2 * i + 1);
+	}
+
+	// prepare ground-truth
+
+	row_t r0(n);
+	for (index_t j = 0; j < n; ++j)
+	{
+		double sab = 0.0;
+		double saa = 0.0;
+		double sbb = 0.0;
+
+		for (index_t i = 0; i < m; ++i)
+		{
+			sab += A(i, j) * B(i, j);
+			saa += A(i, j) * A(i, j);
+			sbb += B(i, j) * B(i, j);
+		}
+		r0[j] = sab / (sqrt(saa) * sqrt(sbb));
+	}
+
+	// test
+
+	row_t r = nrmdot(A, B, colwise());
+	ASSERT_EQ( r.nrows(), 1 );
+	ASSERT_EQ( r.ncolumns(), n );
+	ASSERT_VEC_APPROX( n, r, r0, 1.0e-12 );
+}
+
+
+MN_CASE( rowwise_reduce, nrmdot )
+{
+	typedef dense_matrix<double, M, N> mat_t;
+	typedef dense_col<double, M> col_t;
+
+	const index_t m = M == 0 ? DM : M;
+	const index_t n = N == 0 ? DN : N;
+
+	mat_t A(m, n);
+	mat_t B(m, n);
+	for (index_t i = 0; i < m * n; ++i)
+	{
+		A[i] = double(i + 1);
+		B[i] = double(2 * i + 1);
+	}
+
+	// prepare ground-truth
+
+	col_t r0(m);
+	for (index_t i = 0; i < m; ++i)
+	{
+		double sab = 0.0;
+		double saa = 0.0;
+		double sbb = 0.0;
+
+		for (index_t j = 0; j < n; ++j)
+		{
+			sab += A(i, j) * B(i, j);
+			saa += A(i, j) * A(i, j);
+			sbb += B(i, j) * B(i, j);
+		}
+		r0[i] = sab / (sqrt(saa) * sqrt(sbb));
+	}
+
+	// test
+
+	col_t r = nrmdot(A, B, rowwise());
+	ASSERT_EQ( r.nrows(), m );
+	ASSERT_EQ( r.ncolumns(), 1 );
+	ASSERT_VEC_APPROX( m, r, r0, 1.0e-12 );
+}
+
+
 
 BEGIN_TPACK( colwise_sum )
 	ADD_MN_CASE_3X3( colwise_reduce, sum, DM, DN )
@@ -121,12 +688,106 @@ BEGIN_TPACK( colwise_mean )
 	ADD_MN_CASE_3X3( colwise_reduce, mean, DM, DN )
 END_TPACK
 
+BEGIN_TPACK( rowwise_mean )
+	ADD_MN_CASE_3X3( colwise_reduce, mean, DM, DN )
+END_TPACK
+
+BEGIN_TPACK( colwise_maximum )
+	ADD_MN_CASE_3X3( colwise_reduce, maximum, DM, DN )
+END_TPACK
+
+BEGIN_TPACK( rowwise_maximum )
+	ADD_MN_CASE_3X3( rowwise_reduce, maximum, DM, DN )
+END_TPACK
+
+BEGIN_TPACK( colwise_minimum )
+	ADD_MN_CASE_3X3( colwise_reduce, minimum, DM, DN )
+END_TPACK
+
+BEGIN_TPACK( rowwise_minimum )
+	ADD_MN_CASE_3X3( rowwise_reduce, minimum, DM, DN )
+END_TPACK
+
+
+BEGIN_TPACK( colwise_dot )
+	ADD_MN_CASE_3X3( colwise_reduce, dot, DM, DN )
+END_TPACK
+
+BEGIN_TPACK( rowwise_dot )
+	ADD_MN_CASE_3X3( colwise_reduce, dot, DM, DN )
+END_TPACK
+
+BEGIN_TPACK( colwise_L1norm )
+	ADD_MN_CASE_3X3( colwise_reduce, L1norm, DM, DN )
+END_TPACK
+
+BEGIN_TPACK( rowwise_L1norm )
+	ADD_MN_CASE_3X3( colwise_reduce, L1norm, DM, DN )
+END_TPACK
+
+BEGIN_TPACK( colwise_sqL2norm )
+	ADD_MN_CASE_3X3( colwise_reduce, sqL2norm, DM, DN )
+END_TPACK
+
+BEGIN_TPACK( rowwise_sqL2norm )
+	ADD_MN_CASE_3X3( colwise_reduce, sqL2norm, DM, DN )
+END_TPACK
+
+BEGIN_TPACK( colwise_L2norm )
+	ADD_MN_CASE_3X3( colwise_reduce, L2norm, DM, DN )
+END_TPACK
+
+BEGIN_TPACK( rowwise_L2norm )
+	ADD_MN_CASE_3X3( colwise_reduce, L2norm, DM, DN )
+END_TPACK
+
+BEGIN_TPACK( colwise_Linfnorm )
+	ADD_MN_CASE_3X3( colwise_reduce, Linfnorm, DM, DN )
+END_TPACK
+
+BEGIN_TPACK( rowwise_Linfnorm )
+	ADD_MN_CASE_3X3( colwise_reduce, Linfnorm, DM, DN )
+END_TPACK
+
+BEGIN_TPACK( colwise_nrmdot )
+	ADD_MN_CASE_3X3( colwise_reduce, nrmdot, DM, DN )
+END_TPACK
+
+BEGIN_TPACK( rowwise_nrmdot )
+	ADD_MN_CASE_3X3( rowwise_reduce, nrmdot, DM, DN )
+END_TPACK
+
 
 BEGIN_MAIN_SUITE
 	ADD_TPACK( colwise_sum )
 	ADD_TPACK( rowwise_sum )
 
 	ADD_TPACK( colwise_mean )
+	ADD_TPACK( rowwise_mean )
+
+	ADD_TPACK( colwise_maximum )
+	ADD_TPACK( rowwise_maximum )
+
+	ADD_TPACK( colwise_minimum )
+	ADD_TPACK( rowwise_minimum )
+
+	ADD_TPACK( colwise_dot )
+	ADD_TPACK( rowwise_dot )
+
+	ADD_TPACK( colwise_L1norm )
+	ADD_TPACK( rowwise_L1norm )
+
+	ADD_TPACK( colwise_sqL2norm )
+	ADD_TPACK( rowwise_sqL2norm )
+
+	ADD_TPACK( colwise_L2norm )
+	ADD_TPACK( rowwise_L2norm )
+
+	ADD_TPACK( colwise_Linfnorm )
+	ADD_TPACK( rowwise_Linfnorm )
+
+	ADD_TPACK( colwise_nrmdot )
+	ADD_TPACK( rowwise_nrmdot )
 END_MAIN_SUITE
 
 
