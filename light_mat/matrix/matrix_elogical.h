f3288ff96c0f9061d66adc4493a344ea4483039b
Dahua Lin <lindahua@gmail.com>
2012-08-01 20:44:47 -0400

    new expr framework passed test_mat_elogical

--- light_mat/matrix/matrix_elogical.h
+++ light_mat/matrix/matrix_elogical.h
@@ -22,7 +22,9 @@ namespace lmat
 
 	template<typename T, class Mat>
 	LMAT_ENSURE_INLINE
-	inline typename unary_ewise_expr_map<mask_not_op<T>, Mat>::type
+	inline typename unary_expr_map<
+		ewise_t<mask_not_op<T> >,
+		ref_arg_holder<Mat> >::type
 	operator ~ (const IMatrixXpr<Mat, mask_t<T> >& A)
 	{
 		return ewise(mask_not_op<T>(), A.derived());
@@ -32,7 +34,10 @@ namespace lmat
 
 	template<typename T, class LMat, class RMat>
 	LMAT_ENSURE_INLINE
-	inline typename binary_ewise_expr_map<mask_and_op<T>, LMat, RMat>::type
+	inline typename binary_expr_map<
+		ewise_t<mask_and_op<T> >,
+		ref_arg_holder<LMat>,
+		ref_arg_holder<RMat> >::type
 	operator & (const IMatrixXpr<LMat, mask_t<T> >& A, const IMatrixXpr<RMat, mask_t<T> >& B)
 	{
 		return ewise(mask_and_op<T>(), A.derived(), B.derived());
@@ -42,7 +47,10 @@ namespace lmat
 
 	template<typename T, class LMat, class RMat>
 	LMAT_ENSURE_INLINE
-	inline typename binary_ewise_expr_map<mask_or_op<T>, LMat, RMat>::type
+	inline typename binary_expr_map<
+		ewise_t<mask_or_op<T> >,
+		ref_arg_holder<LMat>,
+		ref_arg_holder<RMat> >::type
 	operator | (const IMatrixXpr<LMat, mask_t<T> >& A, const IMatrixXpr<RMat, mask_t<T> >& B)
 	{
 		return ewise(mask_or_op<T>(), A.derived(), B.derived());
@@ -52,7 +60,10 @@ namespace lmat
 
 	template<typename T, class LMat, class RMat>
 	LMAT_ENSURE_INLINE
-	inline typename binary_ewise_expr_map<mask_xor_op<T>, LMat, RMat>::type
+	inline typename binary_expr_map<
+		ewise_t<mask_xor_op<T> >,
+		ref_arg_holder<LMat>,
+		ref_arg_holder<RMat> >::type
 	operator ^ (const IMatrixXpr<LMat, mask_t<T> >& A, const IMatrixXpr<RMat, mask_t<T> >& B)
 	{
 		return ewise(mask_xor_op<T>(), A.derived(), B.derived());
