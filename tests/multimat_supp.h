999f7948bb36dc40c72c8a8be47428677d970267
Dahua Lin <lindahua@gmail.com>
2012-11-28 10:22:37 -0600

    extract multimat_supp to simplify testing codes

---
+++ tests/multimat_supp.h
@@ -0,0 +1,100 @@
+/**
+ * @file multimat_supp.h
+ *
+ * @brief Support the testing of multiple matrix type
+ *
+ * @author Dahua Lin
+ */
+
+#ifndef MULTIMAT_SUPP_H_
+#define MULTIMAT_SUPP_H_
+
+#include <light_mat/matrix/matrix_classes.h>
+
+using namespace lmat;
+
+#ifndef DEFAULT_M_VALUE
+#define DEFAULT_M_VALUE 7
+#endif
+
+#ifndef DEFAULT_N_VALUE
+#define DEFAULT_N_VALUE 8
+#endif
+
+const index_t DM = DEFAULT_M_VALUE;
+const index_t DN = DEFAULT_N_VALUE;
+const index_t rs = 2;
+const index_t cs = rs * DEFAULT_M_VALUE + 1;
+const index_t LDim = cs;
+
+template<template<typename T, int R, int C> class ClassT, int M, int N>
+struct mat_maker;
+
+template<int M, int N>
+struct mat_maker<ref_matrix, M, N>
+{
+	typedef cref_matrix<double, M, N> cmat_t;
+	typedef ref_matrix<double, M, N> mat_t;
+
+	static index_t max_size(index_t m, index_t n)
+	{
+		return m * n;
+	}
+
+	static cmat_t get_cmat(const double *p, index_t m, index_t n)
+	{
+		return cmat_t(p, m, n);
+	}
+
+	static mat_t get_mat(double *p, index_t m, index_t n)
+	{
+		return mat_t(p, m, n);
+	}
+};
+
+template<int M, int N>
+struct mat_maker<ref_block, M, N>
+{
+	typedef cref_block<double, M, N> cmat_t;
+	typedef ref_block<double, M, N> mat_t;
+
+	static index_t max_size(index_t m, index_t n)
+	{
+		return LDim * n;
+	}
+
+	static cmat_t get_cmat(const double *p, index_t m, index_t n)
+	{
+		return cmat_t(p, m, n, cs);
+	}
+
+	static mat_t get_mat(double *p, index_t m, index_t n)
+	{
+		return mat_t(p, m, n, cs);
+	}
+};
+
+template<int M, int N>
+struct mat_maker<ref_grid, M, N>
+{
+	typedef cref_grid<double, M, N> cmat_t;
+	typedef ref_grid<double, M, N> mat_t;
+
+	static index_t max_size(index_t m, index_t n)
+	{
+		return LDim * n;
+	}
+
+	static cmat_t get_cmat(const double *p, index_t m, index_t n)
+	{
+		return cmat_t(p, m, n, rs, cs);
+	}
+
+	static mat_t get_mat(double *p, index_t m, index_t n)
+	{
+		return mat_t(p, m, n, rs, cs);
+	}
+};
+
+
+#endif /* MULTIMAT_SUPP_H_ */
--- tests/test_mat_arith.cpp
+++ tests/test_mat_arith.cpp
@@ -7,88 +7,13 @@
  */
 
 #include "test_base.h"
+#include "multimat_supp.h"
 
-#include <light_mat/matrix/matrix_classes.h>
 #include <light_mat/matexpr/matrix_arith.h>
 #include <light_mat/matexpr/matrix_ewise_eval.h>
 
 using namespace lmat;
 using namespace lmat::test;
-
-const index_t DM = 7;
-const index_t DN = 8;
-const index_t rs = 2;
-const index_t cs = 15;
-const index_t LDim = 15;
-
-template<template<typename T, int R, int C> class ClassT, int M, int N>
-struct mat_maker;
-
-template<int M, int N>
-struct mat_maker<ref_matrix, M, N>
-{
-	typedef cref_matrix<double, M, N> cmat_t;
-	typedef ref_matrix<double, M, N> mat_t;
-
-	static index_t max_size(index_t m, index_t n)
-	{
-		return m * n;
-	}
-
-	static cmat_t get_cmat(const double *p, index_t m, index_t n)
-	{
-		return cmat_t(p, m, n);
-	}
-
-	static mat_t get_mat(double *p, index_t m, index_t n)
-	{
-		return mat_t(p, m, n);
-	}
-};
-
-template<int M, int N>
-struct mat_maker<ref_block, M, N>
-{
-	typedef cref_block<double, M, N> cmat_t;
-	typedef ref_block<double, M, N> mat_t;
-
-	static index_t max_size(index_t m, index_t n)
-	{
-		return LDim * n;
-	}
-
-	static cmat_t get_cmat(const double *p, index_t m, index_t n)
-	{
-		return cmat_t(p, m, n, cs);
-	}
-
-	static mat_t get_mat(double *p, index_t m, index_t n)
-	{
-		return mat_t(p, m, n, cs);
-	}
-};
-
-template<int M, int N>
-struct mat_maker<ref_grid, M, N>
-{
-	typedef cref_grid<double, M, N> cmat_t;
-	typedef ref_grid<double, M, N> mat_t;
-
-	static index_t max_size(index_t m, index_t n)
-	{
-		return LDim * n;
-	}
-
-	static cmat_t get_cmat(const double *p, index_t m, index_t n)
-	{
-		return cmat_t(p, m, n, rs, cs);
-	}
-
-	static mat_t get_mat(double *p, index_t m, index_t n)
-	{
-		return mat_t(p, m, n, rs, cs);
-	}
-};
 
 typedef dense_matrix<double> dmat_t;
 
