df14e3f76e71f6e818ed98bead4bf73df508d2dc
Dahua Lin <lindahua@gmail.com>
2012-12-22 17:26:05 -0600

    BLAS Level 3 tested

--- light_mat/linalg/blas_l3.h
+++ light_mat/linalg/blas_l3.h
@@ -15,6 +15,8 @@
 
 namespace lmat { namespace blas {
 
+	// gemm
+
 	namespace internal
 	{
 		template<class A>
@@ -100,6 +102,212 @@ namespace lmat { namespace blas {
 	          IRegularMatrix<C, double>& c, char transa='N', char transb='N')
 	{
 		gemm(1.0, a, b, 0.0, c, transa, transb);
+	}
+
+
+	// symm
+
+	namespace internal
+	{
+		template<class A, class B, class C>
+		LMAT_ENSURE_INLINE
+		inline void spmm_get_dims(const A& a, const B& b, const C& c, char side,
+				blas_int& m, blas_int& n)
+		{
+			index_t ma = a.nrows();
+			index_t na = a.ncolumns();
+
+			index_t mb = b.nrows();
+			index_t nb = b.ncolumns();
+
+			if (side == 'L' || side == 'l')
+			{
+				LMAT_CHECK_DIMS( ma == na && na == mb && ma == c.nrows() && nb == c.ncolumns() );
+
+				m = (blas_int)ma;
+				n = (blas_int)nb;
+			}
+			else
+			{
+				LMAT_CHECK_DIMS( ma == na && ma == nb && mb == c.nrows() && na == c.ncolumns());
+
+				m = mb;
+				n = na;
+			}
+		}
+
+		template<class A, class B>
+		LMAT_ENSURE_INLINE
+		inline void spmm_get_dims(const A& a, const B& b, char side,
+				blas_int& m, blas_int& n)
+		{
+			index_t ma = a.nrows();
+			index_t na = a.ncolumns();
+
+			index_t mb = b.nrows();
+			index_t nb = b.ncolumns();
+
+			if (side == 'L' || side == 'l')
+			{
+				LMAT_CHECK_DIMS( ma == na && na == mb );
+
+				m = (blas_int)ma;
+				n = (blas_int)nb;
+			}
+			else
+			{
+				LMAT_CHECK_DIMS( ma == na && ma == nb );
+
+				m = mb;
+				n = na;
+			}
+		}
+	}
+
+	template<class A, class B, class C>
+	LMAT_ENSURE_INLINE
+	inline void symm(float alpha, const IRegularMatrix<A, float>& a, const IRegularMatrix<B, float>& b,
+	                 float beta, IRegularMatrix<C, float>& c, char side='L', char uplo='L')
+	{
+		blas_int m, n;
+		internal::spmm_get_dims(a, b, c, side, m, n);
+
+		blas_int lda = (blas_int)a.col_stride();
+		blas_int ldb = (blas_int)b.col_stride();
+		blas_int ldc = (blas_int)c.col_stride();
+
+		LMAT_BLAS_NAME(ssymm)(&side, &uplo, &m, &n,
+				&alpha, a.ptr_data(), &lda, b.ptr_data(), &ldb, &beta, c.ptr_data(), &ldc);
+	}
+
+	template<class A, class B, class C>
+	LMAT_ENSURE_INLINE
+	inline void symm(double alpha, const IRegularMatrix<A, double>& a, const IRegularMatrix<B, double>& b,
+	                 double beta, IRegularMatrix<C, double>& c, char side='L', char uplo='L')
+	{
+		blas_int m, n;
+		internal::spmm_get_dims(a, b, c, side, m, n);
+
+		blas_int lda = (blas_int)a.col_stride();
+		blas_int ldb = (blas_int)b.col_stride();
+		blas_int ldc = (blas_int)c.col_stride();
+
+		LMAT_BLAS_NAME(dsymm)(&side, &uplo, &m, &n,
+				&alpha, a.ptr_data(), &lda, b.ptr_data(), &ldb, &beta, c.ptr_data(), &ldc);
+	}
+
+	template<class A, class B, class C>
+	LMAT_ENSURE_INLINE
+	inline void symm(const IRegularMatrix<A, float>& a, const IRegularMatrix<B, float>& b,
+			         IRegularMatrix<C, float>& c, char side='L', char uplo='L')
+	{
+		symm(1.0f, a, b, 0.0f, c, side, uplo);
+	}
+
+	template<class A, class B, class C>
+	LMAT_ENSURE_INLINE
+	inline void symm(const IRegularMatrix<A, double>& a, const IRegularMatrix<B, double>& b,
+			         IRegularMatrix<C, double>& c, char side='L', char uplo='L')
+	{
+		symm(1.0f, a, b, 0.0f, c, side, uplo);
+	}
+
+
+	// trmm
+
+	template<class A, class B>
+	LMAT_ENSURE_INLINE
+	inline void trmm(float alpha, const IRegularMatrix<A, float>& a, IRegularMatrix<B, float>& b,
+	                 const trs& ts, char side='L', char uplo='L')
+	{
+		blas_int m, n;
+		internal::spmm_get_dims(a, b, side, m, n);
+
+		blas_int lda = (blas_int)a.col_stride();
+		blas_int ldb = (blas_int)b.col_stride();
+
+		LMAT_BLAS_NAME(strmm)(&side, &(ts.uplo), &(ts.trans), &(ts.diag),
+				&m, &n, &alpha, a.ptr_data(), &lda, b.ptr_data(), &ldb);
+	}
+
+	template<class A, class B>
+	LMAT_ENSURE_INLINE
+	inline void trmm(double alpha, const IRegularMatrix<A, double>& a, IRegularMatrix<B, double>& b,
+	                 const trs& ts, char side='L', char uplo='L')
+	{
+		blas_int m, n;
+		internal::spmm_get_dims(a, b, side, m, n);
+
+		blas_int lda = (blas_int)a.col_stride();
+		blas_int ldb = (blas_int)b.col_stride();
+
+		LMAT_BLAS_NAME(dtrmm)(&side, &(ts.uplo), &(ts.trans), &(ts.diag),
+				&m, &n, &alpha, a.ptr_data(), &lda, b.ptr_data(), &ldb);
+	}
+
+	template<class A, class B>
+	LMAT_ENSURE_INLINE
+	inline void trmm(const IRegularMatrix<A, float>& a, IRegularMatrix<B, float>& b,
+	                 const trs& ts, char side='L', char uplo='L')
+	{
+		trmm(1.0f, a, b, ts, side, uplo);
+	}
+
+	template<class A, class B>
+	LMAT_ENSURE_INLINE
+	inline void trmm(const IRegularMatrix<A, double>& a, IRegularMatrix<B, double>& b,
+	                 const trs& ts, char side='L', char uplo='L')
+	{
+		trmm(1.0, a, b, ts, side, uplo);
+	}
+
+
+	// trsm
+
+	template<class A, class B>
+	LMAT_ENSURE_INLINE
+	inline void trsm(float alpha, const IRegularMatrix<A, float>& a, IRegularMatrix<B, float>& b,
+	                 const trs& ts, char side='L', char uplo='L')
+	{
+		blas_int m, n;
+		internal::spmm_get_dims(a, b, side, m, n);
+
+		blas_int lda = (blas_int)a.col_stride();
+		blas_int ldb = (blas_int)b.col_stride();
+
+		LMAT_BLAS_NAME(strsm)(&side, &(ts.uplo), &(ts.trans), &(ts.diag),
+				&m, &n, &alpha, a.ptr_data(), &lda, b.ptr_data(), &ldb);
+	}
+
+	template<class A, class B>
+	LMAT_ENSURE_INLINE
+	inline void trsm(double alpha, const IRegularMatrix<A, double>& a, IRegularMatrix<B, double>& b,
+	                 const trs& ts, char side='L', char uplo='L')
+	{
+		blas_int m, n;
+		internal::spmm_get_dims(a, b, side, m, n);
+
+		blas_int lda = (blas_int)a.col_stride();
+		blas_int ldb = (blas_int)b.col_stride();
+
+		LMAT_BLAS_NAME(dtrsm)(&side, &(ts.uplo), &(ts.trans), &(ts.diag),
+				&m, &n, &alpha, a.ptr_data(), &lda, b.ptr_data(), &ldb);
+	}
+
+	template<class A, class B>
+	LMAT_ENSURE_INLINE
+	inline void trsm(const IRegularMatrix<A, float>& a, IRegularMatrix<B, float>& b,
+	                 const trs& ts, char side='L', char uplo='L')
+	{
+		trsm(1.0f, a, b, ts, side, uplo);
+	}
+
+	template<class A, class B>
+	LMAT_ENSURE_INLINE
+	inline void trsm(const IRegularMatrix<A, double>& a, IRegularMatrix<B, double>& b,
+	                 const trs& ts, char side='L', char uplo='L')
+	{
+		trsm(1.0, a, b, ts, side, uplo);
 	}
 
 
--- tests/linalg/test_blas_l3.cpp
+++ tests/linalg/test_blas_l3.cpp
@@ -175,6 +175,323 @@ void test_gemm_tt()
 }
 
 
+template<class SA, class SB, class SC, typename T, int M, int N>
+void test_symm_l()
+{
+	index_t m = M == 0 ? DM : M;
+	index_t n = N == 0 ? DN : N;
+
+	typedef typename mat_host<SA, T, M, M>::cmat_t amat_t;
+	typedef typename mat_host<SB, T, M, N>::cmat_t bmat_t;
+	typedef typename mat_host<SC, T, M, N>::mat_t  cmat_t;
+
+	mat_host<SA, T, M, M> a_host(m, m);
+	mat_host<SB, T, M, N> b_host(m, n);
+	mat_host<SC, T, M, N> c_host(m, n);
+
+	typename mat_host<SA, T, M, M>::mat_t amat = a_host.get_mat();
+	fill_rand_sym(amat);
+	b_host.fill_rand();
+
+	amat_t a = a_host.get_cmat();
+	bmat_t b = b_host.get_cmat();
+	cmat_t c = c_host.get_mat();
+
+	dense_matrix<T> r(m, n);
+	T tol = blas_default_tol<T>::get();
+
+	safe_mm(T(1), a, 'n', b, 'n', T(0), c, r);
+	blas::symm(a, b, c);
+
+	ASSERT_MAT_APPROX(m, n, c, r, tol);
+
+	T alpha = T(2.5);
+	T beta = T(1.6);
+
+	safe_mm(alpha, a, 'n', b, 'n', beta, c, r);
+	blas::symm(alpha, a, b, beta, c);
+
+	ASSERT_MAT_APPROX(m, n, c, r, tol);
+}
+
+template<class SA, class SB, class SC, typename T, int M, int N>
+void test_symm_r()
+{
+	index_t m = M == 0 ? DM : M;
+	index_t n = N == 0 ? DN : N;
+
+	typedef typename mat_host<SA, T, N, N>::cmat_t amat_t;
+	typedef typename mat_host<SB, T, M, N>::cmat_t bmat_t;
+	typedef typename mat_host<SC, T, M, N>::mat_t  cmat_t;
+
+	mat_host<SA, T, N, N> a_host(n, n);
+	mat_host<SB, T, M, N> b_host(m, n);
+	mat_host<SC, T, M, N> c_host(m, n);
+
+	typename mat_host<SA, T, N, N>::mat_t amat = a_host.get_mat();
+	fill_rand_sym(amat);
+	b_host.fill_rand();
+
+	amat_t a = a_host.get_cmat();
+	bmat_t b = b_host.get_cmat();
+	cmat_t c = c_host.get_mat();
+
+	dense_matrix<T> r(m, n);
+	T tol = blas_default_tol<T>::get();
+
+	safe_mm(T(1), b, 'n', a, 'n', T(0), c, r);
+	blas::symm(a, b, c, 'r');
+
+	ASSERT_MAT_APPROX(m, n, c, r, tol);
+
+	T alpha = T(2.5);
+	T beta = T(1.6);
+
+	safe_mm(alpha, b, 'n', a, 'n', beta, c, r);
+	blas::symm(alpha, a, b, beta, c, 'r');
+
+	ASSERT_MAT_APPROX(m, n, c, r, tol);
+}
+
+
+template<class SA, class SB, class SC, typename T, int M, int N>
+void test_trmm_ln(char uplo)
+{
+	index_t m = M == 0 ? DM : M;
+	index_t n = N == 0 ? DN : N;
+
+	typedef typename mat_host<SA, T, M, M>::cmat_t amat_t;
+	typedef typename mat_host<SB, T, M, N>::mat_t bmat_t;
+
+	mat_host<SA, T, M, M> a_host(m, m);
+	mat_host<SB, T, M, N> b_host(m, n);
+
+	typename mat_host<SA, T, M, M>::mat_t amat = a_host.get_mat();
+	fill_rand_tri(amat, uplo);
+	b_host.fill_rand();
+
+	amat_t a = a_host.get_cmat();
+	bmat_t b = b_host.get_mat();
+
+	dense_matrix<T> r(m, n);
+	T tol = blas_default_tol<T>::get();
+
+	safe_mm(T(1), a, 'n', b, 'n', T(0), b, r);
+	blas::trmm(a, b, blas::trs(uplo));
+
+	ASSERT_MAT_APPROX(m, n, b, r, tol);
+}
+
+template<class SA, class SB, class SC, typename T, int M, int N>
+void test_trmm_lt(char uplo)
+{
+	index_t m = M == 0 ? DM : M;
+	index_t n = N == 0 ? DN : N;
+
+	typedef typename mat_host<SA, T, M, M>::cmat_t amat_t;
+	typedef typename mat_host<SB, T, M, N>::mat_t bmat_t;
+
+	mat_host<SA, T, M, M> a_host(m, m);
+	mat_host<SB, T, M, N> b_host(m, n);
+
+	typename mat_host<SA, T, M, M>::mat_t amat = a_host.get_mat();
+	fill_rand_tri(amat, uplo);
+	b_host.fill_rand();
+
+	amat_t a = a_host.get_cmat();
+	bmat_t b = b_host.get_mat();
+
+	dense_matrix<T> r(m, n);
+	T tol = blas_default_tol<T>::get();
+
+	safe_mm(T(1), a, 't', b, 'n', T(0), b, r);
+	blas::trmm(a, b, blas::trs(uplo, 't'));
+
+	ASSERT_MAT_APPROX(m, n, b, r, tol);
+}
+
+template<class SA, class SB, class SC, typename T, int M, int N>
+void test_trmm_rn(char uplo)
+{
+	index_t m = M == 0 ? DM : M;
+	index_t n = N == 0 ? DN : N;
+
+	typedef typename mat_host<SA, T, N, N>::cmat_t amat_t;
+	typedef typename mat_host<SB, T, M, N>::mat_t bmat_t;
+
+	mat_host<SA, T, N, N> a_host(n, n);
+	mat_host<SB, T, M, N> b_host(m, n);
+
+	typename mat_host<SA, T, N, N>::mat_t amat = a_host.get_mat();
+	fill_rand_tri(amat, uplo);
+	b_host.fill_rand();
+
+	amat_t a = a_host.get_cmat();
+	bmat_t b = b_host.get_mat();
+
+	dense_matrix<T> r(m, n);
+	T tol = blas_default_tol<T>::get();
+
+	safe_mm(T(1), b, 'n', a, 'n', T(0), b, r);
+	blas::trmm(a, b, blas::trs(uplo), 'r');
+
+	ASSERT_MAT_APPROX(m, n, b, r, tol);
+}
+
+
+template<class SA, class SB, class SC, typename T, int M, int N>
+void test_trmm_rt(char uplo)
+{
+	index_t m = M == 0 ? DM : M;
+	index_t n = N == 0 ? DN : N;
+
+	typedef typename mat_host<SA, T, N, N>::cmat_t amat_t;
+	typedef typename mat_host<SB, T, M, N>::mat_t bmat_t;
+
+	mat_host<SA, T, N, N> a_host(n, n);
+	mat_host<SB, T, M, N> b_host(m, n);
+
+	typename mat_host<SA, T, N, N>::mat_t amat = a_host.get_mat();
+	fill_rand_tri(amat, uplo);
+	b_host.fill_rand();
+
+	amat_t a = a_host.get_cmat();
+	bmat_t b = b_host.get_mat();
+
+	dense_matrix<T> r(m, n);
+	T tol = blas_default_tol<T>::get();
+
+	safe_mm(T(1), b, 'n', a, 't', T(0), b, r);
+	blas::trmm(a, b, blas::trs(uplo, 't'), 'r');
+
+	ASSERT_MAT_APPROX(m, n, b, r, tol);
+}
+
+
+template<class SA, class SB, class SC, typename T, int M, int N>
+void test_trsm_ln(char uplo)
+{
+	index_t m = M == 0 ? DM : M;
+	index_t n = N == 0 ? DN : N;
+
+	typedef typename mat_host<SA, T, M, M>::cmat_t amat_t;
+	typedef typename mat_host<SB, T, M, N>::mat_t bmat_t;
+
+	mat_host<SA, T, M, M> a_host(m, m);
+	mat_host<SB, T, M, N> b_host(m, n);
+
+	T tol = blas_default_tol<T>::get() * T(5);
+
+	typename mat_host<SA, T, M, M>::mat_t amat = a_host.get_mat();
+	fill_rand_tri(amat, uplo);
+	b_host.fill_rand();
+
+	amat_t a = a_host.get_cmat();
+	bmat_t b = b_host.get_mat();
+
+	dense_matrix<T> r = b;
+	dense_matrix<T> p(m, n, zero());
+
+	blas::trsm(a, b, blas::trs(uplo));
+	safe_mm(T(1), a, 'n', b, 'n', T(0), b, p);
+
+	ASSERT_MAT_APPROX(m, n, p, r, tol);
+}
+
+
+template<class SA, class SB, class SC, typename T, int M, int N>
+void test_trsm_lt(char uplo)
+{
+	index_t m = M == 0 ? DM : M;
+	index_t n = N == 0 ? DN : N;
+
+	typedef typename mat_host<SA, T, M, M>::cmat_t amat_t;
+	typedef typename mat_host<SB, T, M, N>::mat_t bmat_t;
+
+	mat_host<SA, T, M, M> a_host(m, m);
+	mat_host<SB, T, M, N> b_host(m, n);
+
+	T tol = blas_default_tol<T>::get() * T(5);
+
+	typename mat_host<SA, T, M, M>::mat_t amat = a_host.get_mat();
+	fill_rand_tri(amat, uplo);
+	b_host.fill_rand();
+
+	amat_t a = a_host.get_cmat();
+	bmat_t b = b_host.get_mat();
+
+	dense_matrix<T> r = b;
+	dense_matrix<T> p(m, n, zero());
+
+	blas::trsm(a, b, blas::trs(uplo, 't'));
+	safe_mm(T(1), a, 't', b, 'n', T(0), b, p);
+
+	ASSERT_MAT_APPROX(m, n, p, r, tol);
+}
+
+template<class SA, class SB, class SC, typename T, int M, int N>
+void test_trsm_rn(char uplo)
+{
+	index_t m = M == 0 ? DM : M;
+	index_t n = N == 0 ? DN : N;
+
+	typedef typename mat_host<SA, T, N, N>::cmat_t amat_t;
+	typedef typename mat_host<SB, T, M, N>::mat_t bmat_t;
+
+	mat_host<SA, T, N, N> a_host(n, n);
+	mat_host<SB, T, M, N> b_host(m, n);
+
+	T tol = blas_default_tol<T>::get() * T(5);
+
+	typename mat_host<SA, T, N, N>::mat_t amat = a_host.get_mat();
+	fill_rand_tri(amat, uplo);
+	b_host.fill_rand();
+
+	amat_t a = a_host.get_cmat();
+	bmat_t b = b_host.get_mat();
+
+	dense_matrix<T> r = b;
+	dense_matrix<T> p(m, n, zero());
+
+	blas::trsm(a, b, blas::trs(uplo), 'r');
+	safe_mm(T(1), b, 'n', a, 'n', T(0), b, p);
+
+	ASSERT_MAT_APPROX(m, n, p, r, tol);
+}
+
+template<class SA, class SB, class SC, typename T, int M, int N>
+void test_trsm_rt(char uplo)
+{
+	index_t m = M == 0 ? DM : M;
+	index_t n = N == 0 ? DN : N;
+
+	typedef typename mat_host<SA, T, N, N>::cmat_t amat_t;
+	typedef typename mat_host<SB, T, M, N>::mat_t bmat_t;
+
+	mat_host<SA, T, N, N> a_host(n, n);
+	mat_host<SB, T, M, N> b_host(m, n);
+
+	T tol = blas_default_tol<T>::get() * T(5);
+
+	typename mat_host<SA, T, N, N>::mat_t amat = a_host.get_mat();
+	fill_rand_tri(amat, uplo);
+	b_host.fill_rand();
+
+	amat_t a = a_host.get_cmat();
+	bmat_t b = b_host.get_mat();
+
+	dense_matrix<T> r = b;
+	dense_matrix<T> p(m, n, zero());
+
+	blas::trsm(a, b, blas::trs(uplo, 't'), 'r');
+	safe_mm(T(1), b, 'n', a, 't', T(0), b, p);
+
+	ASSERT_MAT_APPROX(m, n, p, r, tol);
+}
+
+
+
+
 #define DEF_BLAS3_CASES_P3( Name ) \
 	TMN_CASE( mat_blas3, Name##_ccc ) { test_##Name<cont, cont, cont, T, M, N>(); } \
 	TMN_CASE( mat_blas3, Name##_ccb ) { test_##Name<cont, cont, bloc, T, M, N>(); } \
@@ -184,6 +501,16 @@ void test_gemm_tt()
 	TMN_CASE( mat_blas3, Name##_bcb ) { test_##Name<bloc, cont, bloc, T, M, N>(); } \
 	TMN_CASE( mat_blas3, Name##_bbc ) { test_##Name<bloc, bloc, cont, T, M, N>(); } \
 	TMN_CASE( mat_blas3, Name##_bbb ) { test_##Name<bloc, bloc, bloc, T, M, N>(); }
+
+#define DEF_BLAS3_CASES_P3_TR( Name ) \
+	TMN_CASE( mat_blas3, Name##_ccc ) { test_##Name<cont, cont, cont, T, M, N>('l'); test_##Name<cont, cont, cont, T, M, N>('u'); } \
+	TMN_CASE( mat_blas3, Name##_ccb ) { test_##Name<cont, cont, bloc, T, M, N>('l'); test_##Name<cont, cont, bloc, T, M, N>('u'); } \
+	TMN_CASE( mat_blas3, Name##_cbc ) { test_##Name<cont, bloc, cont, T, M, N>('l'); test_##Name<cont, bloc, cont, T, M, N>('u'); } \
+	TMN_CASE( mat_blas3, Name##_cbb ) { test_##Name<cont, bloc, bloc, T, M, N>('l'); test_##Name<cont, bloc, bloc, T, M, N>('u'); } \
+	TMN_CASE( mat_blas3, Name##_bcc ) { test_##Name<bloc, cont, cont, T, M, N>('l'); test_##Name<bloc, cont, cont, T, M, N>('u'); } \
+	TMN_CASE( mat_blas3, Name##_bcb ) { test_##Name<bloc, cont, bloc, T, M, N>('l'); test_##Name<bloc, cont, bloc, T, M, N>('u'); } \
+	TMN_CASE( mat_blas3, Name##_bbc ) { test_##Name<bloc, bloc, cont, T, M, N>('l'); test_##Name<bloc, bloc, cont, T, M, N>('u'); } \
+	TMN_CASE( mat_blas3, Name##_bbb ) { test_##Name<bloc, bloc, bloc, T, M, N>('l'); test_##Name<bloc, bloc, bloc, T, M, N>('u'); }
 
 #define ADD_BLAS3_CASES_P3( Name, T ) \
 	ADD_TMN_CASE_3X3( mat_blas3, Name##_ccc, T, DM, DN ); \
@@ -239,6 +566,115 @@ BEGIN_TPACK( mat_gemm_tt_double )
 END_TPACK
 
 
+// symm
+
+DEF_BLAS3_CASES_P3( symm_l )
+
+BEGIN_TPACK( mat_symm_l_float )
+	ADD_BLAS3_CASES_P3( symm_l, float )
+END_TPACK
+
+BEGIN_TPACK( mat_symm_l_double )
+	ADD_BLAS3_CASES_P3( symm_l, double )
+END_TPACK
+
+DEF_BLAS3_CASES_P3( symm_r )
+
+BEGIN_TPACK( mat_symm_r_float )
+	ADD_BLAS3_CASES_P3( symm_r, float )
+END_TPACK
+
+BEGIN_TPACK( mat_symm_r_double )
+	ADD_BLAS3_CASES_P3( symm_r, double )
+END_TPACK
+
+
+// trmm
+
+DEF_BLAS3_CASES_P3_TR( trmm_ln )
+
+BEGIN_TPACK( mat_trmm_ln_float )
+	ADD_BLAS3_CASES_P3( trmm_ln, float )
+END_TPACK
+
+BEGIN_TPACK( mat_trmm_ln_double )
+	ADD_BLAS3_CASES_P3( trmm_ln, double )
+END_TPACK
+
+DEF_BLAS3_CASES_P3_TR( trmm_lt )
+
+BEGIN_TPACK( mat_trmm_lt_float )
+	ADD_BLAS3_CASES_P3( trmm_lt, float )
+END_TPACK
+
+BEGIN_TPACK( mat_trmm_lt_double )
+	ADD_BLAS3_CASES_P3( trmm_lt, double )
+END_TPACK
+
+DEF_BLAS3_CASES_P3_TR( trmm_rn )
+
+BEGIN_TPACK( mat_trmm_rn_float )
+	ADD_BLAS3_CASES_P3( trmm_rn, float )
+END_TPACK
+
+BEGIN_TPACK( mat_trmm_rn_double )
+	ADD_BLAS3_CASES_P3( trmm_rn, double )
+END_TPACK
+
+DEF_BLAS3_CASES_P3_TR( trmm_rt )
+
+BEGIN_TPACK( mat_trmm_rt_float )
+	ADD_BLAS3_CASES_P3( trmm_rt, float )
+END_TPACK
+
+BEGIN_TPACK( mat_trmm_rt_double )
+	ADD_BLAS3_CASES_P3( trmm_rt, double )
+END_TPACK
+
+
+// tsmm
+
+DEF_BLAS3_CASES_P3_TR( trsm_ln )
+
+BEGIN_TPACK( mat_trsm_ln_float )
+	ADD_BLAS3_CASES_P3( trsm_ln, float )
+END_TPACK
+
+BEGIN_TPACK( mat_trsm_ln_double )
+	ADD_BLAS3_CASES_P3( trsm_ln, double )
+END_TPACK
+
+DEF_BLAS3_CASES_P3_TR( trsm_lt )
+
+BEGIN_TPACK( mat_trsm_lt_float )
+	ADD_BLAS3_CASES_P3( trsm_lt, float )
+END_TPACK
+
+BEGIN_TPACK( mat_trsm_lt_double )
+	ADD_BLAS3_CASES_P3( trsm_lt, double )
+END_TPACK
+
+DEF_BLAS3_CASES_P3_TR( trsm_rn )
+
+BEGIN_TPACK( mat_trsm_rn_float )
+	ADD_BLAS3_CASES_P3( trsm_rn, float )
+END_TPACK
+
+BEGIN_TPACK( mat_trsm_rn_double )
+	ADD_BLAS3_CASES_P3( trsm_rn, double )
+END_TPACK
+
+DEF_BLAS3_CASES_P3_TR( trsm_rt )
+
+BEGIN_TPACK( mat_trsm_rt_float )
+	ADD_BLAS3_CASES_P3( trsm_rt, float )
+END_TPACK
+
+BEGIN_TPACK( mat_trsm_rt_double )
+	ADD_BLAS3_CASES_P3( trsm_rt, double )
+END_TPACK
+
+
 BEGIN_MAIN_SUITE
 	ADD_TPACK( mat_gemm_nn_float )
 	ADD_TPACK( mat_gemm_nn_double )
@@ -248,6 +684,29 @@ BEGIN_MAIN_SUITE
 	ADD_TPACK( mat_gemm_tn_double )
 	ADD_TPACK( mat_gemm_tt_float )
 	ADD_TPACK( mat_gemm_tt_double )
+
+	ADD_TPACK( mat_symm_l_float )
+	ADD_TPACK( mat_symm_l_double )
+	ADD_TPACK( mat_symm_r_float )
+	ADD_TPACK( mat_symm_r_double )
+
+	ADD_TPACK( mat_trmm_ln_float )
+	ADD_TPACK( mat_trmm_ln_double )
+	ADD_TPACK( mat_trmm_lt_float )
+	ADD_TPACK( mat_trmm_lt_double )
+	ADD_TPACK( mat_trmm_rn_float )
+	ADD_TPACK( mat_trmm_rn_double )
+	ADD_TPACK( mat_trmm_rt_float )
+	ADD_TPACK( mat_trmm_rt_double )
+
+	ADD_TPACK( mat_trsm_ln_float )
+	ADD_TPACK( mat_trsm_ln_double )
+	ADD_TPACK( mat_trsm_lt_float )
+	ADD_TPACK( mat_trsm_lt_double )
+	ADD_TPACK( mat_trsm_rn_float )
+	ADD_TPACK( mat_trsm_rn_double )
+	ADD_TPACK( mat_trsm_rt_float )
+	ADD_TPACK( mat_trsm_rt_double )
 END_MAIN_SUITE
 
 
