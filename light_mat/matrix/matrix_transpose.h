ee6e7d0a4b752a231bacf08ea180b8060d458e64
Dahua Lin <lindahua@gmail.com>
2012-12-26 17:40:55 -0600

    test_transpose_expr passed

--- light_mat/matrix/matrix_transpose.h
+++ light_mat/matrix/matrix_transpose.h
@@ -13,6 +13,12 @@
 
 namespace lmat
 {
+	// forward declaration
+
+	template<class Arg> class transpose_expr;
+
+	// direct transpose
+
 	template<typename T, class SMat, class DMat>
 	LMAT_ENSURE_INLINE
 	inline void transpose(const IRegularMatrix<SMat, T>& smat, IRegularMatrix<DMat, T>& dmat)
@@ -25,6 +31,92 @@ namespace lmat
 		internal::direct_transpose(m, n, smat, dmat);
 	}
 
+
+	/******************************************************
+	 *
+	 *  transpose expression class
+	 *
+	 ******************************************************/
+
+	template<class Arg>
+	struct matrix_traits<transpose_expr<Arg> >
+	{
+		static const int num_dimensions = 2;
+		static const int ct_num_rows = meta::ncols<Arg>::value;
+		static const int ct_num_cols = meta::nrows<Arg>::value;
+
+		static const bool is_readonly = true;
+
+		typedef typename matrix_traits<Arg>::value_type value_type;
+		typedef typename matrix_traits<Arg>::domain domain;
+	};
+
+
+	template<class Arg>
+	class transpose_expr
+	: public IMatrixXpr<transpose_expr<Arg>, typename matrix_traits<Arg>::value_type>
+	{
+		static_assert(meta::is_regular_mat<Arg>::value, "Arg must be a regular matrix.");
+		typedef matrix_shape<meta::ncols<Arg>::value, meta::nrows<Arg>::value> shape_type;
+
+	public:
+		LMAT_ENSURE_INLINE
+		explicit transpose_expr(const Arg& arg) : m_arg(arg) { }
+
+		LMAT_ENSURE_INLINE
+		const Arg& arg() const
+		{
+			return m_arg;
+		}
+
+		LMAT_ENSURE_INLINE index_t nelems() const
+		{
+			return m_arg.nelems();
+		}
+
+		LMAT_ENSURE_INLINE index_t nrows() const
+		{
+			return m_arg.ncolumns();
+		}
+
+		LMAT_ENSURE_INLINE index_t ncolumns() const
+		{
+			return m_arg.nrows();
+		}
+
+		LMAT_ENSURE_INLINE shape_type shape() const
+		{
+			return shape_type(nrows(), ncolumns());
+		}
+
+	private:
+		const Arg& m_arg;
+	};
+
+
+	template<typename T, class Arg>
+	LMAT_ENSURE_INLINE
+	inline transpose_expr<Arg> transpose(const IRegularMatrix<Arg, T>& arg)
+	{
+		return transpose_expr<Arg>(arg.derived());
+	}
+
+
+	/******************************************************
+	 *
+	 *  evaluation
+	 *
+	 ******************************************************/
+
+	template<class Arg, class DMat>
+	LMAT_ENSURE_INLINE
+	inline void evaluate(const transpose_expr<Arg>& expr,
+			IRegularMatrix<DMat, typename matrix_traits<Arg>::value_type>& dmat)
+	{
+		transpose(expr.arg(), dmat);
+	}
+
+
 }
 
-#endif /* MAT_TRANSPOSE_H_ */
+#endif
--- tests/matrix/test_transpose_expr.cpp
+++ tests/matrix/test_transpose_expr.cpp
@@ -1,16 +1,15 @@
 /**
- * @file test_mat_transpose.cpp
+ * @file test_transpose_expr.cpp
  *
- * Unit testing for matrix transpose
+ * Unit testing for matrix transpose expression
  *
  * @author Dahua Lin
  */
 
-#include "test_base.h"
-#include "multimat_supp.h"
+#include "../test_base.h"
+#include "../multimat_supp.h"
 
-#include <light_mat/matexpr/matrix_arith.h>
-#include <light_mat/matexpr/matrix_transpose.h>
+#include <light_mat/matrix/matrix_transpose.h>
 #include <string>
 
 using namespace lmat;
@@ -51,7 +50,7 @@ void test_mat_transpose()
 	mat_t dmat = dst.get_mat();
 
 	src.fill_lin();
-	dmat = trans(smat);
+	dmat = transpose(smat);
 
 	dense_matrix<double, N, M> rmat(n, m);
 	for (index_t i = 0; i < m; ++i)
@@ -73,7 +72,7 @@ void test_mat_transpose()
 	mat2_t dmat2 = dst2.get_mat();
 
 	src2.fill_lin();
-	dmat2 = trans(smat2);
+	dmat2 = transpose(smat2);
 
 	dense_matrix<double, M, N> rmat2(m, n);
 	for (index_t i = 0; i < m; ++i)
@@ -85,69 +84,10 @@ void test_mat_transpose()
 }
 
 
-template<class Tag2, int M, int N>
-void test_xpr_transpose()
-{
-	const index_t m = M == 0 ? DM : M;
-	const index_t n = N == 0 ? DN : N;
-
-	// M x N ==> N x M
-
-	typedef typename mat_host<cont, double, M, N>::cmat_t cmat_t;
-	typedef typename mat_host<Tag2, double, N, M>::mat_t mat_t;
-
-	mat_host<cont, double, M, N> src(m, n);
-	mat_host<Tag2, double, N, M> dst(n, m);
-
-	cmat_t smat = src.get_cmat();
-	mat_t dmat = dst.get_mat();
-
-	src.fill_lin();
-	dmat = trans(smat * 2.0 + 1.0);
-
-	dense_matrix<double, N, M> rmat(n, m);
-	for (index_t i = 0; i < m; ++i)
-	{
-		for (index_t j = 0; j < n; ++j) rmat(j, i) = smat(i, j) * 2.0 + 1.0;
-	}
-
-	ASSERT_MAT_EQ(n, m, dmat, rmat);
-
-	// N x M ==> M x N
-
-	typedef typename mat_host<cont, double, N, M>::cmat_t cmat2_t;
-	typedef typename mat_host<Tag2, double, M, N>::mat_t mat2_t;
-
-	mat_host<cont, double, N, M> src2(n, m);
-	mat_host<Tag2, double, M, N> dst2(m, n);
-
-	cmat2_t smat2 = src2.get_cmat();
-	mat2_t dmat2 = dst2.get_mat();
-
-	src2.fill_lin();
-	dmat2 = trans(smat2 * 2.0 + 1.0);
-
-	dense_matrix<double, M, N> rmat2(m, n);
-	for (index_t i = 0; i < m; ++i)
-	{
-		for (index_t j = 0; j < n; ++j) rmat2(i, j) = smat2(j, i) * 2.0 + 1.0;
-	}
-
-	ASSERT_MAT_EQ(m, n, dmat2, rmat2);
-}
-
-
 
 #define TEST_MAT_TRANS( sform, dform, name ) \
 	MN_CASE( mat_trans, name ) \
 	{ test_mat_transpose<sform, dform, M, N>(); } \
-	BEGIN_TPACK( mat_trans_##name ) \
-	ADD_MN_CASE_3X3( mat_trans, name, DM, DN ) \
-	END_TPACK
-
-#define TEST_XPR_TRANS( dform, name ) \
-	MN_CASE( mat_trans, name ) \
-	{ test_xpr_transpose<dform, M, N>(); } \
 	BEGIN_TPACK( mat_trans_##name ) \
 	ADD_MN_CASE_3X3( mat_trans, name, DM, DN ) \
 	END_TPACK
@@ -164,9 +104,6 @@ TEST_MAT_TRANS( grid, cont, grid_to_mat )
 TEST_MAT_TRANS( grid, bloc, grid_to_blk )
 TEST_MAT_TRANS( grid, grid, grid_to_grid )
 
-TEST_XPR_TRANS( cont, xpr_to_mat )
-TEST_XPR_TRANS( bloc, xpr_to_blk )
-TEST_XPR_TRANS( grid, xpr_to_grid )
 
 BEGIN_MAIN_SUITE
 	ADD_TPACK( mat_trans_mat_to_mat )
@@ -180,9 +117,5 @@ BEGIN_MAIN_SUITE
 	ADD_TPACK( mat_trans_grid_to_mat )
 	ADD_TPACK( mat_trans_grid_to_blk )
 	ADD_TPACK( mat_trans_grid_to_grid )
-
-	ADD_TPACK( mat_trans_xpr_to_mat )
-	ADD_TPACK( mat_trans_xpr_to_blk )
-	ADD_TPACK( mat_trans_xpr_to_grid )
 END_MAIN_SUITE
 
